16dbdb59d6004f6742746825ecb35011095f1462
patbc <culleyp@oregonstate.edu>
2022-02-27 18:03:01 -0600

    intro macro added

--- Proj6_culleyp.asm
+++ Proj6_culleyp.asm
@@ -1,35 +1,51 @@
-TITLE Program #6     (Proj6_culleyp.asm)
+TITLE Low-level I/O procedures     (Proj6_culleyp.asm)
 
 ; Author: Patrick Culley
 ; Last Modified: 2/27/2022 
 ; OSU email address: culleyp@oregonstate.edu
 ; Course number/section:   CS271 Section 400
 ; Project Number: #6       Due Date: 3/13/2022
-; Description: Project #6 
+; Description: Designing low-level I/O procedures 
 
 
 INCLUDE Irvine32.inc
 
-; (insert macro definitions here)
+mDisplayString	 MACRO	 chars					; displays strings using WriteString method 
+  PUSH		EDX 
+  MOV		EDX, chars 
+  CALL		WriteString 
+  POP		EDX 
+ENDM 
 
-; (insert constant definitions here)
+mGetString		 MACRO	 chars					; prints prompt to user and gathers integer 
+  PUSH		EDX 
+  PUSH		EAX 
+  mDisplayString	chars
+  POP		EAX
+  POP		EDX
+ENDM 
+
 
 .data
+intro1		BYTE	 "Programming assignment 6: Designing low-level I/O procedures",13,10,
+					 "Written by: Patrick Culley",13,10,13,10,0
+intro2		BYTE	 "Please provide 10 signed decimal integers.",13,10,
+					 "Each number needs to be small enough to fit inside a 32 bit register. After you have finished",13,10,
+					 "inputting the raw numbers I will display a list of the integers, their sum, and their average",13,10, 
+					 "value",13,10,13,10,0
+prompt		BYTE	 "Please enter a signed integer: ",13,10
 
-StringA   BYTE  "This is a 26 byte string!",0
-StringB   BYTE  26 DUP(0)
 
 .code
 main PROC
-  LOCAL	 sumLoop 
-  MOV    ESI, OFFSET StringA
-  MOV    EDI, OFFSET StringB
-  MOV    ECX, LENGTHOF StringA
+  mDisplayString	OFFSET intro1				; print intro1 to screen 
   
-  REP    MOVSB   ; Copies all of StringA into StringB
-  MOV	 EDX, OFFSET StringB  
-  CALL	 WriteString
-  INVOKE ExitProcess,0	; exit to operating system
+  mDisplayString	OFFSET intro2				; print intro2 to screen 
+
+  mGetString		OFFSET prompt				; print prompt and read/store user-entered integer 
+
+  INVOKE ExitProcess,0							; exit to operating system
 main ENDP
+
 
 END main
