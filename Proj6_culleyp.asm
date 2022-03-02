1895b35a7acf76a2cf2a3241f0969080f9a2ff11
patbc <culleyp@oregonstate.edu>
2022-03-01 20:04:15 -0600

    added character loop

--- Proj6_culleyp.asm
+++ Proj6_culleyp.asm
@@ -17,13 +17,18 @@ mDisplayString	 MACRO	 chars					; displays strings using WriteString method
   POP		EDX 
 ENDM 
 
-mGetString		 MACRO	 chars, num					; prints prompt to user and gathers integer 
+mGetString		 MACRO	 chars, input, count, size				; prints prompt to user and gathers integer 
   PUSH		EDX 
   PUSH		EAX 
-  mDisplayString	chars
-  CALL		ReadChar
-  MOV		userNum, AL 
-  CALL		WriteChar
+  PUSH		ECX
+  MOV		EDX, chars
+  CALL		WriteString
+  MOV		EDX, input
+  MOV		ECX, size 
+  CALL		ReadString
+  MOV		EDI, count 
+  MOV		[EDI], EAX 
+  POP		ECX 
   POP		EAX
   POP		EDX
 ENDM 
@@ -37,19 +42,91 @@ intro2		BYTE	 "Please provide 10 signed decimal integers.",13,10,
 					 "inputting the raw numbers I will display a list of the integers, their sum, and their average",13,10, 
 					 "value",13,10,13,10,0
 prompt		BYTE	 "Please enter a signed integer: ",0
-userNum		BYTE	  ? 
+invalidMsg	BYTE	 "ERROR: You did not enter a signed number or your number was too big.",13,10,
+					 "Please try again: ",0
+inputNum	BYTE	  11 DUP (?)
+counter		DWORD	  ? 
+inputSize	DWORD	  11 
 
 
 .code
 main PROC
-  mDisplayString	OFFSET intro1						; print intro1 to screen 
-  
-  mDisplayString	OFFSET intro2						; print intro2 to screen 
+  mDisplayString	OFFSET intro1													; print intro1 to screen  
+  mDisplayString	OFFSET intro2													; print intro2 to screen 
 
-  mGetString		OFFSET prompt, OFFSET userNum		; print prompt and read/store user-entered integer 
+  PUSH		OFFSET invalidMsg
+  PUSH		inputSize
+  PUSH		OFFSET prompt 
+  PUSH		OFFSET inputNum
+  PUSH		OFFSET counter 
+  CALL		ReadVal 
 
-  INVOKE ExitProcess,0									; exit to operating system
+  INVOKE	ExitProcess,0																; exit to operating system
 main ENDP
+
+
+ReadVal		PROC
+  PUSH		EBP 
+  MOV		EBP, ESP 
+  PUSH		ECX
+  PUSH		ESI
+  PUSH		EBX 
+  PUSH		EAX 
+  PUSH		EDX
+
+  mGetString  [EBP + 16], [EBP + 12], [EBP + 8], [EBP + 20]
+  PUSH		EBX 
+  MOV		ESI, [EBP + 12]				; move character array to ESI 
+  MOV		EBX, [EBP + 8]				 
+  MOV		ECX, [EBX]					; move count of characters to ECX
+  POP		EBX 
+
+_loopChars:	
+  LODSB														; compares if number is in-range from 48 to 57
+  PUSH		EBX
+  MOV		EBX, [EBP + 8]
+  CMP		ECX, [EBX]										; compare counter to length
+  JE		_checkSign										; compare if first char is negative 
+  CMP		AL, 48
+  JB		_invalidInput
+  CMP		AL, 57
+  JA		_invalidInput
+  LOOP		_loopChars
+  JMP		_theEnd 
+
+_checkSign:
+  POP		EBX
+  CMP		AL, 45 
+  JE		_changeSign 
+  CMP		AL, 43 
+  JE		_changeSign
+  JMP		_invalidInput 
+
+_changeSign: 
+  LOOP		_loopChars
+
+
+_invalidInput: 
+  mGetString  [EBP + 24], [EBP + 12], [EBP + 8], [EBP + 20]
+  PUSH		EBX 
+  MOV		ESI, [EBP + 12]				; move character array to ESI 
+  MOV		EBX, [EBP + 8]				 
+  MOV		ECX, [EBX]					; move count of characters to ECX
+  POP		EBX 
+  JMP		_loopChars 
+
+
+_theEnd:
+  POP		EDX 
+  POP		EAX 
+  POP		EBX 
+  POP		ESI 
+  POP		ECX
+  POP		EBP 
+  RET  20
+ReadVal		ENDP 
+
+
 
 
 END main
